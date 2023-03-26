c7febe254775d61515891d200e7eaaf527fddaec
Isabela <isabela.ribeiro@outlook.com>
2023-03-26 18:17:19 -0300

    NOW I CONTROL THE SNAKEEE

--- snake.py
+++ snake.py
@@ -8,7 +8,7 @@ LEFT = 3
 
 pygame.init()
 screen = pygame.display.set_mode((600,600))
-pygame.display.set_caption('Snake')
+pygame.display.set_caption('Snake Game')
 
 snake = [(200, 200),(210, 200), (220, 200)]
 snake_skin = pygame.Surface((10,10))
@@ -28,15 +28,25 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             exit()
+        if event.type == KEYDOWN:
+            if event.key == K_UP:
+                my_direction = UP
+            if event.key == K_DOWN:
+                my_direction = DOWN
+            if event.key == K_LEFT:
+                my_direction = LEFT
+            if event.key == K_RIGHT:
+                my_direction = RIGHT
+                
             
     if my_direction ==  UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
     if my_direction ==  DOWN:
         snake[0] = (snake[0][0], snake[0][1] + 10)
     if my_direction ==  RIGHT:
-        snake[0] = (snake[0][0] - 10, snake[0][1])
+        snake[0] = (snake[0][0] + 10, snake[0][1])
     if my_direction ==  LEFT:
-        snake[0] = (snake[0][0] + 10, snake[0][1])        
+        snake[0] = (snake[0][0] - 10, snake[0][1])        
         
     for i in range(len(snake) - 1, 0, -1):
         snake[i] = (snake[i-1][0], snake[i-1][1])
