a47540b4489186cecdf3a9b0cdf1894d3d4f000a
Isabela <isabela.ribeiro@outlook.com>
2023-03-26 18:34:30 -0300

    now the snake grows

--- snake.py
+++ snake.py
@@ -1,6 +1,14 @@
 import pygame, random
 from pygame.locals import *
 
+def on_grid_random():
+    x = random.randint(0,590)
+    y = random.randint(0,590)
+    return (x//10 * 10, y//10 * 10)
+
+def collision(c1, c2):
+    return (c1[0] == c2[0]) and (c1[1] == c2[1])
+
 UP = 0
 DOWN = 1
 RIGHT = 2
@@ -14,7 +22,7 @@ snake = [(200, 200),(210, 200), (220, 200)]
 snake_skin = pygame.Surface((10,10))
 snake_skin.fill((255,255,255))
 
-food_pos = (random.randint(0,590), random.randint(0,590))
+food_pos = on_grid_random()
 food = pygame.Surface((10,10))
 food.fill((255,0,0))
 
@@ -23,7 +31,7 @@ my_direction = LEFT
 clock = pygame.time.Clock()
 
 while True:
-    clock.tick(10)
+    clock.tick(20)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -39,6 +47,13 @@ while True:
                 my_direction = RIGHT
                 
             
+    if collision(snake[0], food_pos):
+        food_pos = on_grid_random()
+        snake.append((0,0))
+        
+    for i in range(len(snake) - 1, 0, -1):
+        snake[i] = (snake[i-1][0], snake[i-1][1])
+
     if my_direction ==  UP:
         snake[0] = (snake[0][0], snake[0][1] - 10)
     if my_direction ==  DOWN:
@@ -46,11 +61,8 @@ while True:
     if my_direction ==  RIGHT:
         snake[0] = (snake[0][0] + 10, snake[0][1])
     if my_direction ==  LEFT:
-        snake[0] = (snake[0][0] - 10, snake[0][1])        
-        
-    for i in range(len(snake) - 1, 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
-            
+        snake[0] = (snake[0][0] - 10, snake[0][1])            
+               
     screen.fill((0,0,0))
     screen.blit(food, food_pos)
     
