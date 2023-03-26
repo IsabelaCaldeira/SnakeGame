3ab8dae48b255a34ad8e56bb0f48cf2afe7e9258
Isabela <isabela.ribeiro@outlook.com>
2023-03-26 17:35:29 -0300

    adding snake and coordenations'

--- snake.py
+++ snake.py
@@ -1,13 +1,20 @@
 import pygame 
 from pygame.locals import *
 
+UP = 0
+DOWN = 1
+RIGHT = 2
+LEFT = 3
+
 pygame.init()
 screen = pygame.display.set_mode((600,600))
 pygame.display.set_caption('Snake')
 
+snake = [(200, 200),(210, 200), (220, 200)]
+my_direction = LEFT
 
 while True:
-    
+    0
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
