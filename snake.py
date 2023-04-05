0818de6e3902f7491d9266c5eb196e4bcf78450a
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-04-04 22:34:47 -0300

    Changing Pontuacao to Score

--- snake.py
+++ snake.py
@@ -99,7 +99,7 @@ while not game_over:
     for y in range(0, 600, 10): 
         pygame.draw.line(screen, (40, 40, 40), (0, y), (600, y))
     
-    score_font = font.render('Pontuação: %s' % (score), True, (255,255,255))
+    score_font = font.render('Score: %s' % (score), True, (255,255,255))
     score_rect = score_font.get_rect()
     score_rect.topleft = (20, 10)
     screen.blit(score_font, score_rect)
