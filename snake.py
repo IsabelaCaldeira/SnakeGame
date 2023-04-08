7032b606719d65cd62d2f0f9a39312e5342dfc2b
Isabela Caldeira <isabela.ribeiro@outlook.com>
2023-04-08 07:00:52 -0300

    Changing the snake to green

--- snake.py
+++ snake.py
@@ -76,7 +76,7 @@ def restart_game():
 def start_game():
     snake = [(200, 200),(210, 200), (220, 200)]
     snake_skin = pygame.Surface((10,10))
-    snake_skin.fill((255,255,255))
+    snake_skin.fill((34,139,34))
     snake_speed = 20
 
     food_pos = on_grid_random()
